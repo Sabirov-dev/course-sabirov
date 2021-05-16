08ea807874fe1cfbd9f992dde6fee9218139d912
BlaiseCri <sabirov.webdeveloper@gmail.com>
2021-05-16 13:51:03 +0800

    my hw

---
+++ module2-solution/js/scrip.js
@@ -0,0 +1,24 @@
+// function Circle (radius){
+//     this.radius = radius;
+// }
+// Circle.prototype.getArea = 
+//     function (){
+//         return Math.PI * Math.pow(this.radius, 2);
+//     };
+// var myCircle = new Circle(10);
+// console.log(myCircle.getArea());
+
+// var litCircle = {
+//     radius: 10,
+
+//     getArea: function (){
+//         var self = this;
+//         var incRadius = function (){
+//             self.radius = 20;
+//         };
+//         incRadius();
+//         return Math.PI * Math.pow(this.radius, 2);
+//     }
+// };
+// console.log(litCircle.getArea());
+
